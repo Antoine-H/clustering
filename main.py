45434c760e30044a65e1bbb9a384db2dd20ff955
huchet <huchet@maison-ordi-portable.home>
2018-01-16 00:05:57 +0100

    Updated comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import operator
 import math
 
 
-# Read dataset, store tweets as [timestamp, lat, long]
+# Reads dataset, stores tweets as [timestamp, latitude, longitude].
 def read_tweets (file):
     tweets=[]
     with open(file) as input:
@@ -15,27 +15,27 @@ def read_tweets (file):
     return tweets
 
 
-# Compute distance between two tweets
+# Computes distance between two tweets.
 def tweet_dist(v1, v2):
     return math.sqrt((v2[2] - v1[2]) ** 2
                   + ((v2[1] - v1[1]) ** 2))
 
 
-# Compute bounds on a tweet set.
-# Start from a random tweet, compute the distance to the rest of the
-# tweets. Repeat from farthest/shortest tweet until we get the same
-# two tweets. Still quite heuristic... O(n) randomized.
+# Computes bounds on a tweet set.
+# Starts from a random tweet, computes the distance to the rest of the
+# tweets. Repeats from farthest/shortest tweet until it gets the same
+# two tweets. Still quite heuristic... Maybe sufficient. O(n) randomized.
 def bound(graph, comp):
 
     i = random.sample(range(len(graph)),3)
 
-    # Init bound
+    # Initial bound.
     bnd = tweet_dist(graph[i[0]], graph[i[1]])
 
-    # Random starting point
+    # Random starting point.
     v1      = graph[i[2]]
     v1_prev = [0.0, 0.0, 0.0]
-    # Init vnext in case we get the bound at first try
+    # Initialise vnext in case we get the bound at first try.
     vnext   = [0.0, 0.0, 0.0]
     v2      = [0.0, 0.0, 0.0]
 
@@ -44,7 +44,7 @@ def bound(graph, comp):
         #print (v1, v1_prev)
         for v2 in graph:
             dist = tweet_dist(v1,v2)
-            # Unlucky if bnd starts better than all dists
+            # Unlucky if bnd starts better than all dists.
             if comp(dist,bnd) and v1 != v2:
                 #print(bnd, dist)
                 bnd   = dist
@@ -56,7 +56,7 @@ def bound(graph, comp):
 
     return bnd
 
-# Computes the closest pair of points in a tweet set in O(n log n)
+# Computes the closest pair of points in a tweet set in O(n log n).
 # Misses closest pair each on one side of the median
 # Quite slow...
 def closest_pair_of_points (graph):
@@ -74,11 +74,11 @@ def closest_pair_of_points (graph):
         #delta  = min(closest_pair_of_points(graph[median:]),
         #             closest_pair_of_points(graph[:median]))
 
-        # Get the tweets within delta of the border
+        # Get the tweets within delta of the border.
         border = (graph[median:median+6]
                 + graph[median-6:median])
 
-        # Distance to median
+        # Distance to median.
         def cmp (v1, v2):
             return (tweet_dist (graph[median], v1)
                   - tweet_dist (graph[median], v2))
