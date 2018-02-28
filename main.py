6e4eb0760c0f0c7c8e5f5f931d7ed25766d6ea5a
antoine <antoine@huchet.me>
2018-02-28 20:54:37 +0100

    Moved from list to set for better performances

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
-
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+#
 #
 # Implementation of a 2-approximation algorithm for the fully dynamic k-center
 # clustering problem.
 # From https://sites.google.com/site/maurosozio/techReport.pdf
 #
-# TODO : closest_pair_of_points
-#        change lists to dictionnaries
-#
+
 
 import random
 import operator
 import math
+import cartopy.crs       as ccrs
+import matplotlib.pyplot as plt
+import timeit
+import sys
 
 
 # Reads dataset, stores tweets as [timestamp, latitude, longitude].
 def read_tweets (file):
-    tweets=[]
+    tweets = []
     with open(file) as input:
         for line in input:
-            tweets.append([float(x) for x in line.split()])
+            #tweets.append([float(x) for x in line.split()])
+            tweets.append((float(line.split()[0]),
+                           float(line.split()[1]),
+                           float(line.split()[2])))
     return tweets
 
 
+# Plots clusters
+def plot(clusters):
+    map = plt.axes(projection=ccrs.PlateCarree())
+    map.stock_img()
+    for cluster in clusters:
+        colourR = (random.random(), random.random(), random.random())
+        map.plot([tweet[1] for tweet in cluster],
+                 [tweet[2] for tweet in cluster],
+                 "o",
+                 color = colourR,
+                 markersize = 1,
+                 transform = ccrs.Geodetic())
+    plt.savefig("map.png")
+    #plt.show()
+
+
 # Computes distance between two tweets.
-def tweet_dist(v1, v2):
+def tweet_dist (v1, v2):
+    #update dmin, dmax
     return math.sqrt((v2[2] - v1[2]) ** 2
                   + ((v2[1] - v1[1]) ** 2))
 
@@ -32,7 +56,7 @@ def tweet_dist(v1, v2):
 # Starts from a random tweet, computes the distance to the rest of the
 # tweets. Repeats from farthest/shortest tweet until it gets the same
 # two tweets. Still quite heuristic... Maybe sufficient. O(n) randomized.
-def bound(graph, comp):
+def bound (graph, comp):
 
     rand_nodes = [graph[i] for i in random.sample(range(len(graph)),3)]
 
@@ -59,39 +83,6 @@ def bound(graph, comp):
     return bnd
 
 
-# Computes the closest pair of points in a tweet set in O(n log n).
-# Misses closest pair each on one side of the median.
-# From https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
-# Quite slow... NOT DONE YET, point on each side of the border.
-def closest_pair_of_points (graph):
-
-    if len(graph) == 1:
-        # Arbitrary..
-        return 100
-    if len(graph) == 2:
-        return tweet_dist(graph[0], graph[1])
-    else:
-        sorted_lat  = graph.sort(key=operator.itemgetter(1))
-        sorted_long = graph.sort(key=operator.itemgetter(2))
-
-        median      = int(len(graph)/2)
-
-        #delta  = min(closest_pair_of_points(graph[median:]),
-        #             closest_pair_of_points(graph[:median]))
-
-        # Get the tweets within delta of the border.
-        border = (graph[median:median+6]
-                + graph[median-6:median])
-
-        # Distance to median.
-        def cmp (v1, v2):
-            return (tweet_dist (graph[median], v1)
-                  - tweet_dist (graph[median], v2))
-
-    return min(closest_pair_of_points(graph[median:]),
-               closest_pair_of_points(graph[:median]))
-
-
 # Computes βs.
 def betas (dmin, dmax, epsilon):
     n     = (1+epsilon)
@@ -105,10 +96,11 @@ def betas (dmin, dmax, epsilon):
 
 # Builds a cluster within 2β distance of a given center.
 def build_cluster (graph, beta, center):
-    cluster = [center]
+    cluster = set()
+    cluster.add(center)
     for node in graph:
         if tweet_dist(center, node) < 2 * beta:
-            cluster.append(node)
+            cluster.add(node)
     return cluster
 
 
@@ -118,99 +110,219 @@ def clustering (graph, k, betas):
 
     result = []
     for beta in betas:
-        centers     = []
+        centers     = set()
         clusters    = []
-        unclustered = graph[:]
+        unclustered = set(graph)
         nb_centers  = 1
 
+        print(beta, "\t", betas.index(beta)+1, "out of\t", len(betas),
+                flush=True)
         while nb_centers < k and len(unclustered):
 
-            centers.append(random.choice(unclustered))
-            unclustered.remove(centers[-1])
-            clusters.append(build_cluster(unclustered, beta, centers[-1]))
+            #print(nb_centers,"out of\t",k, "unclustered:\t", len(unclustered))
+            new_center = unclustered.pop()
+            centers.add(new_center)
+            clusters.append(build_cluster(unclustered, beta, new_center))
 
-            unclustered = [x for x in unclustered if x not in clusters[-1]]
+            unclustered = set(x for x in unclustered if x not in clusters[-1])
             nb_centers += 1
 
         result.append([centers,clusters,unclustered,beta])
     return result
 
 
+# Returns the index of the the cluster containing the point.
+def get_cluster(clusters, point):
+    for i in range(len(clusters)):
+        if point in clusters[i]:
+            return i
+    return None
+
+
 # Insterts a point in an existing cluster if possible, otherwise as a new
 # center, otherwise as an unclustered point.
-def pointInsertion(L, k, point):
+def pointInsertion (L, k, point):
     for i in range(len(L)):
         centers     = L[i][0]
         clusters    = L[i][1]
         unclustered = L[i][2]
         beta        = L[i][3]
         done        = False
-
         # Inserts in an existing cluster.
         for center in centers:
-            if tweet_dist (point, center) < 2 * beta:
-                i = centers.index(center)
-                clusters[i].append(point)
+            dist = tweet_dist(point, center)
+            if dist < 2 * beta:
+                j = get_cluster(clusters, center)
+                clusters[j].add(point)
                 done = True
-        # Inserts as a new center.
+            # Recomputes bounds.
+            #if dist > dmax or dist < dmin:
+            #    def rebound(dist, L):
+            #        global dmax
+            #        global dmin
+            #        global epsilon
+            #        global k
+            #        if dist > dmax:
+            #            dmax = dist
+            #            lbeta = betas(dmin,dmax,epsilon)
+            #            if len(L) != len(lbeta):
+            #                tmp = set(c for c in clusters)#.union(unclustered)
+            #                print(tmp)
+            #                L.append(clustering(list(tmp),k,[lbeta[len(lbeta)-len(L):]]))
+            #        elif dmin < dist:
+            #            dmin = dist
+            #            lbeta = betas(dmin,dmax,epsilon)
+            #            if len(L) != len(lbeta):
+            #                tmp = set(c for c in clusters).union(unclustered)
+            #                L.append(clustering(list(tmp)),k,[lbeta[:len(lbeta)-len(L)]])
+            #                L.sort(key=lambda l: l[3])
+            #    L = rebound(dist,L)
+        # Or as a new center.
         if len(centers) < k and not done:
-            centers.append(point)
-            clusters.append(build_cluster(unclustered, beta, centers[-1]))
+            centers.add(point)
+            clusters.append(build_cluster(unclustered, beta, point))
         elif not done:
-            unclustered.append(point)
+            unclustered.add(point)
+        L[i] = (centers, clusters, unclustered, beta)
     return L
 
 
-# Flattens a list of lists into a list
-def flatten (lls): return [l for ls in lls for l in ls]
-
 # Deletes a point. If it isn't a center, just deletes it, otherwise reclusters.
-def pointDeletion(L, k, point):
+def pointDeletion (L, k, point):
     for i in range(len(L)):
         centers     = L[i][0]
         clusters    = L[i][1]
         unclustered = L[i][2]
         beta        = L[i][3]
 
-        if point in flatten(clusters) and point not in centers:
-            for cluster in clusters:
-                if point in cluster:
-                    cluster.remove(point)
-        elif point in unclustered:
+        if point in unclustered:
             unclustered.remove(point)
         elif point in centers:
 
-            j        = centers.index(point)
-            new_uncl = flatten(clusters[j:]) + unclustered
+            j        = get_cluster(clusters, point)
+            new_uncl = set(tweet for cluster in clusters[j:]
+                            for tweet in cluster).union(unclustered)
+            centers = centers.difference(new_uncl)
             new_uncl.remove(point)
-            tmp      = clustering (new_uncl, k-j+1, [beta])
-
-            del centers  [j:]
             del clusters [j:]
 
-            centers    += tmp[0][0]
+            tmp      = clustering(new_uncl, k-j+1, [beta])
+
+
+            centers     = centers.union(tmp[0][0])
             clusters   += tmp[0][1]
-            unclustered = tmp[0][2]
+            unclustered = unclustered.union(tmp[0][2])
+        else:
+            for cluster in clusters:
+                if point in cluster:
+                    cluster.remove(point)
+
+        L[i] = (centers, clusters, unclustered, beta)
     return L
 
 
+# Returns the smallest β.
 def get_solution (L):
     for x in L:
         if not x[2]:
             return x
 
 
-# Main
+# Returns the index of the smallest β.
+def get_solution_index (L):
+    for i in range(len(L)):
+        if not L[i][2]:
+            return i
 
-tweets = read_tweets("dataset/twitter_1000000.txt")
 
-#dmin  = closest_pair_of_points(tweets)
-dmin   = bound(tweets, operator.lt)
-dmax   = bound(tweets, operator.gt)
+# Plots the best solution.
+def plot_solution(L):
+    plot(L[get_solution_index(L)][1])
 
-L      = clustering(tweets, 4, betas(dmin, dmax, 0.5))
 
-print(L)
+if __name__ == "__main__":
 
-L = pointDeletion(L,4,[1504866209.0, 12.3267, 45.4386])
+    if len(sys.argv) < 4:
+        print("[*] python3 main.py k epsilon window")
+        sys.exit()
+    global k
+    global epsilon
+    global window
+    global dmin
+    global dmax
+
+    k       = int(sys.argv[1])
+    epsilon = float(sys.argv[2])
+    window  = int(sys.argv[3])
+    if len(sys.argv) > 5:
+        stop = int(sys.argv[5])
+    else:
+        stop = 0
+
+    time  = []
+    best_beta = []
+    start = timeit.default_timer()
+    print("[*] Parsing input file.")
+    checkpoint = timeit.default_timer()
+
+    tweets = read_tweets("dataset/twitter_1000000.txt")
+
+    print("[*] It took:\t\t", timeit.default_timer() - checkpoint)
+    ctime = timeit.default_timer() - start
+    time.append(ctime)
+    print("[*] Time elapsed:\t", ctime)
+
+    print("[*] Computing dmin.")
+    checkpoint = timeit.default_timer()
+
+    dmin   = bound(tweets[:window], operator.lt)
+
+    print("[*] It took:\t\t", timeit.default_timer() - checkpoint)
+    ctime = timeit.default_timer() - start
+    time.append(ctime)
+    print("[*] Time elapsed:\t", ctime)
+    print("[*] Computing dmax.")
+    checkpoint = timeit.default_timer()
+
+    dmax   = bound(tweets[:window], operator.gt)
+
+    print("[*] It took:\t\t", timeit.default_timer() - checkpoint)
+    ctime = timeit.default_timer() - start
+    time.append(ctime)
+    print("[*] Time elapsed:\t", ctime)
+
+    print("[*] Got a dmin of:\t", dmin)
+    print("[*] Got a dmax of:\t", dmax)
+
+    print("[*] Initial clustering.")
+    checkpoint = timeit.default_timer()
+
+    lbetas = betas(dmin,dmax,epsilon)
+    L      = clustering(tweets[:window], k, lbetas)
+
+    print("[*] It took:\t\t", timeit.default_timer() - checkpoint)
+    ctime = timeit.default_timer() - start
+    time.append(ctime)
+    print("[*] Time elapsed:\t", ctime)
+
+    best_beta.append(get_solution(L)[3])
+    count = window
+    if not stop:
+        for i in range(len(tweets)-window):
+            L = pointInsertion(L, k, tweets[i+(window-1)])
+            L = pointDeletion (L, k, tweets[i-(window-1)])
+            best_beta.append(get_solution(L)[3])
+            if count % 10000 == 0:
+                print("[*]", count, "\t\t tweets processed so far.")
+                time.append(ctime)
+                ctime = timeit.default_timer() - start
+                print("[*] Time elapsed:\t", ctime, flush=True)
+            count += 1
+
+    print("[*] Best overall β:\t", get_solution(L)[3])
+    plot_solution(L)
+
+    print(time)
+
+    print(best_beta)
 
